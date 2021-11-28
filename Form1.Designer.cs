f5672d7ff84b12e9f6789eec5cf37e173a2a7508
Darek <darekt@abmicro.pl>
2021-11-28 19:36:29 +0100

    Finished

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -47,6 +47,7 @@ namespace cSharp_WindowsForms_1
             // 
             // textBox1
             // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.textBox1.Location = new System.Drawing.Point(157, 90);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
